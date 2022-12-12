04b6b7a5fc6c61e5f5d0330425cf6e97089aa5e0
sds <2546914721@qq.com>
2022-12-12 20:06:25 +0800

    史东升测试提交

--- src/main/java/com/example/database/DatabaseApplication.java
+++ src/main/java/com/example/database/DatabaseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DatabaseApplication {
 
     public static void main(String[] args) {
+        System.out.println("史东升_pull_test");
         SpringApplication.run(DatabaseApplication.class, args);
     }
 
