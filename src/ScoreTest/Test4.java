d9b0be2f8879cf05c1f2f73f0be94bf581ce4cea
“simple597” <497636083@qq.com>
2022-12-18 10:20:42 +0800

    lyy

--- src/ScoreTest/Test4.java
+++
@@ -1,9 +0,0 @@
-package ScoreTest;
-
-import Dao.DAOFactory;
-
-public class Test4 {
-    public static void main(String[] args) {
-        DAOFactory.getInstance().getScoreDAO().deleteScore("K001","0433");
-    }
-}
--- src/StudentTest/Test4.java
+++
@@ -1,9 +0,0 @@
-package StudentTest;
-
-import Dao.DAOFactory;
-
-public class Test4 {
-    public static void main(String[] args) {
-        DAOFactory.getInstance().getStudentDAO().deleteStudent("张三");
-    }
-}
